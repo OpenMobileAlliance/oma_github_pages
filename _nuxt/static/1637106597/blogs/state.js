ab8234396fca41791ef2f0afafde121740bc0870
jpradocueva <jpradocueva@users.noreply.github.com>
2021-11-18 16:24:33 +0000

    deploy: 601735ebeb64f509bc27e2e33c83c76c96ac2351

--- _nuxt/static/1637106597/blogs/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Foma_github_pages\u002F_nuxt\u002Fstatic\u002F1637106597",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblogs\u002F",config:{_app:{basePath:"\u002Foma_github_pages\u002F",assetsPath:"\u002Foma_github_pages\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bd3c8b02"}}}}(null));
--- _nuxt/static/1637106597/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblogs","\u002Fnews","\u002Fresources","\u002Ffaq","\u002Ftestfests","\u002Fblogs\u002F2020-01-23-Lw2M2M-5G","\u002Fnews\u002F2021-07-8-ENISA","\u002Fblogs\u002F2020-12-09-lwm2m-release","\u002Fnews\u002F2021-05-19-ENISA-lw2m2-wp","\u002Fnews\u002F2021-03-02-uCIFI"]})
