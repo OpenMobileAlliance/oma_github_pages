ab5eef5395ef440ec91a02445ce2e3d701e26a81
rubystream <rubystream@users.noreply.github.com>
2021-11-18 19:44:06 +0000

    deploy: f00e51d4ff0b49a5579d8c3242859be20b6856c1

---
+++ _nuxt/static/1637264639/news/2021-03-02-uCIFI/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637264639",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnews\u002F2021-03-02-uCIFI\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ed25764b"}}}}(null));
