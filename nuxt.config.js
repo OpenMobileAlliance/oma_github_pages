aa6dcaeea84463528fe14095756e72301be132fb
Joaquin <jprado@standardshub.io>
2021-11-18 08:29:50 -0800

    add a new base: "/lwm2m.openmobilealliance.org/",
    Change the value of the base to base: "/lwm2m.openmobilealliance.org/",

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
+    base: "/lwm2m.openmobilealliance.org/",
     trailingSlash: true
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
