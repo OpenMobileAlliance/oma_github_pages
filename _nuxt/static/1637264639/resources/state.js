13987eba0f3bcd63b6984cf516e9413bca8493e8
rubystream <rubystream@users.noreply.github.com>
2021-11-18 20:16:26 +0000

    deploy: aa7cfa8278b365b0d1eea227a3057c9ee9d8eb3e

--- _nuxt/static/1637264639/resources/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637264639",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresources\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ed25764b"}}}}(null));
