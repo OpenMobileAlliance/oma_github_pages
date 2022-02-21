b08522ac9cb9c7db706c88e5752661293aede9e9
jpradocueva <jpradocueva@users.noreply.github.com>
2022-02-21 20:52:20 +0000

    deploy: 9e6d72b03b00174bcc30340bfb0183c1d9dff22f

---
+++ _nuxt/90d8444.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{342:function(n,e,t){"use strict";t.r(e);var o={props:{icon:{type:String,required:!0}}},r=t(50),component=Object(r.a)(o,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("FontAwesomeIcon",{attrs:{icon:n.icon.split(" ")}})}),[],!1,null,null,null);e.default=component.exports}}]);
