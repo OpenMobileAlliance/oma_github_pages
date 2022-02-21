feb44c446dcf72d4d8464b3244144c83424223c7
hannestschofenig <hannestschofenig@users.noreply.github.com>
2022-02-21 12:16:14 +0000

    deploy: 3dc5fd945e82357a4636cd835bdbbac2e5d64e00

--- _nuxt/static/1637272296/faq/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637272296",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffaq\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c94f7288"}}}}(null));
--- _nuxt/static/1637272296/news/2021-07-8-ENISA/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637272296",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnews\u002F2021-07-8-ENISA\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c94f7288"}}}}(null));
