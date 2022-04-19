7202a9ddc9800fbf21e452092c2e94b86183d6fd
hannestschofenig <hannestschofenig@users.noreply.github.com>
2022-04-19 15:33:36 +0000

    deploy: 15437cbe3fc666b00ff11aa36c7574a42881e6dd

--- _nuxt/0893a1c.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{345:function(n,e,t){"use strict";t.r(e);var o={props:{icon:{type:String,required:!0}}},r=t(51),component=Object(r.a)(o,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("font-awesome-icon",{attrs:{icon:n.icon.split(" ")}})}),[],!1,null,null,null);e.default=component.exports}}]);
