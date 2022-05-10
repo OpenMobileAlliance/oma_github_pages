bb25e6879efc59c2166719f8147a13467f8656dd
hannestschofenig <hannestschofenig@users.noreply.github.com>
2022-05-10 15:34:13 +0000

    deploy: af7d330be9673420983426b0b6b74fea1b6e687d

--- _nuxt/bf0b7c7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{356:function(n,e,t){"use strict";t.r(e);var l={},c=t(52),component=Object(c.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("b-container",[n._t("default")],2)}),[],!1,null,null,null);e.default=component.exports}}]);
