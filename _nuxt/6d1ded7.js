01184cb029f48cf13ea6815c4313ff4f73002de2
jmudge <jmudge@users.noreply.github.com>
2022-07-07 14:46:35 +0000

    deploy: 6871200960020ef12f9899243a1a4af24fc3769e

---
+++ _nuxt/6d1ded7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{361:function(n,t,e){"use strict";e.r(t);var l={},c=e(52),component=Object(c.a)(l,(function(){return(0,this._self._c)("b-container",[this._t("default")],2)}),[],!1,null,null,null);t.default=component.exports}}]);
