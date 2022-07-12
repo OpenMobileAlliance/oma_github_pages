1317b3bab4941b650a213e3c3c2a072fd827ccce
Joaquin <jprado@standardshub.io>
2022-07-12 15:19:48 -0500

    add links to social media LinkedIn and Twitter

--- components/Footer.vue
+++ components/Footer.vue
@@ -12,16 +12,15 @@
             <b-nav-item href="https://twitter.com/OMASpecWorks" target="_blank">
               <font-awesome-icon :icon="['fab', 'twitter']" />
             </b-nav-item>
+            <b-nav-item href="https://www.linkedin.com/company/52819/admin/" target="_blank">
+              <font-awesome-icon :icon="['fab', 'linkedin']" />
+            </b-nav-item>
             <b-nav-item
-              href="https://www.facebook.com/OMASpecWorks"
-              target="_blank"
-            >
+              href="https://www.facebook.com/OMASpecWorks" target="_blank">
               <font-awesome-icon :icon="['fab', 'facebook']" />
             </b-nav-item>
             <b-nav-item
-              href="https://github.com/OpenMobileAlliance"
-              target="_blank"
-            >
+              href="https://github.com/OpenMobileAlliance" target="_blank">
               <font-awesome-icon :icon="['fab', 'github']" />
             </b-nav-item>
           </b-nav>
