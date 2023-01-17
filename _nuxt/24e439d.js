194ae67bce1e9c11a5322da6ba6bfd5c72bc5e77
hannestschofenig <hannestschofenig@users.noreply.github.com>
2023-01-17 16:10:16 +0000

    deploy: e625678ad491724c37b765c1eee4a10338e2372b

--- _nuxt/24e439d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{375:function(n,t,e){"use strict";e.r(t);var l={},c=e(52),component=Object(c.a)(l,(function(){return(0,this._self._c)("b-container",[this._t("default")],2)}),[],!1,null,null,null);t.default=component.exports}}]);
